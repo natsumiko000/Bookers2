31f669021e6885dec168d30629182ae002c356d7
natsumiko000 <k.natsu5454@gmail.com>
2020-03-21 20:51:47 +0900

    add-user-devise-routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions:      "users/sessions",
+    registrations: "users/registrations",
+    passwords:     'users/passwords',
+    confirmations: "users/confirmations"
   }
 end
