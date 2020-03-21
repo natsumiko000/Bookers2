121482fc92e87888716627a63ef16e0688e8715c
natsumiko000 <k.natsu5454@gmail.com>
2020-03-21 21:13:44 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  devise_for :users, controllers: {
-    sessions:      "users/sessions",
-    registrations: "users/registrations",
-    passwords:     'users/passwords',
-    confirmations: "users/confirmations"
-  }
+  devise_for :users
+
+  resources :users, only: [:show]
 end
